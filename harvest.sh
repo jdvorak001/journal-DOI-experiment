3bd86eb480abe3585df2109a011ad2be3409836a
jdvorak <jdvorak@vpn88.net.cvut.cz>
2018-04-11 22:54:28 +0200

    save journal metadata into directory data/journals/

--- harvest.sh
+++ harvest.sh
@@ -2,5 +2,5 @@
 cat data/issn.txt | \
 while read ISSN
 do
-  echo wget -nv https://api.crossref.org/journals/$ISSN
+  wget -nv --directory-prefix=data/journals/ https://api.crossref.org/journals/$ISSN
 done
